3d38dcfdf4d0290b418b942e8641525b13160b46
greg@thinktopography.com <greg@thinktopography.com>
2019-02-15 01:33:18 -0500

    bump

--- src/ios/ASWebAuthenticationSession.m
+++ src/ios/ASWebAuthenticationSession.m
@@ -15,7 +15,9 @@ ASWebAuthenticationSession *_authenticationVC;
 
 - (void)start:(CDVInvokedUrlCommand *)command {
 
-    if (@available(iOS 11.0, *)) {
+    printf("hello1");
+    if (@available(iOS 12.0, *)) {
+      printf("hello2");
         NSString* redirectScheme = [command.arguments objectAtIndex:0];
         NSURL* requestURL = [NSURL URLWithString:[command.arguments objectAtIndex:1]];
         ASWebAuthenticationSession* authenticationVC =
